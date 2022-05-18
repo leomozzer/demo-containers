43f682d0c67b1ac2d3e9d9c51ce1897960757c15
leomozzer <mozzer.ferraz@gmailcom>
2022-05-18 17:18:18 -0300

    Module acg and mysql acg

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -1,34 +1,50 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "rg" {
+  name     = "acr-rg"
   location = "West Europe"
 }
 
-resource "azurerm_container_group" "example" {
-  name                = "example-continst"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  ip_address_type     = "Public"
-  os_type             = "Linux"
-
-  image_registry_credential {
-    username = var.acr_username
-    password = var.acr_password
-    server   = var.acr_server
-  }
-
-  container {
-    name   = "api-container"
-    image  = "zorfexkacr.azurecr.io/api:latest"
-    cpu    = "0.5"
-    memory = "1.5"
-
-    ports {
-      port     = 9001
-      protocol = "TCP"
-    }
-  }
-
-  tags = {
-    environment = "testing"
-  }
+module "acg_mysql" {
+  source          = "./modules/acg"
+  acg_name        = "mysql-container"
+  rg_name         = azurerm_resource_group.rg.name
+  rg_location     = azurerm_resource_group.rg.location
+  ip_address_type = "Public"
+  os_type         = "Linux"
+  acr_username    = var.acr_username
+  acr_password    = var.acr_password
+  acr_server      = var.acr_server
+  container_name  = "mysql-container"
+  container_image = "${var.acr_server}/mysql:latest"
+  port            = 3306
+  protocol        = "TCP"
 }
+
+# resource "azurerm_container_group" "example" {
+#   name                = "example-continst"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+#   ip_address_type     = "Public"
+#   os_type             = "Linux"
+
+#   image_registry_credential {
+#     username = var.acr_username
+#     password = var.acr_password
+#     server   = var.acr_server
+#   }
+
+#   container {
+#     name   = "api-container"
+#     image  = "zorfexkacr.azurecr.io/api:latest"
+#     cpu    = "0.5"
+#     memory = "1.5"
+
+#     ports {
+#       port     = 9001
+#       protocol = "TCP"
+#     }
+#   }
+
+#   tags = {
+#     environment = "testing"
+#   }
+# }
