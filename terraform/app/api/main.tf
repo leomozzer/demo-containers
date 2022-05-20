103634440b2719f33d69c0a247ab7718de21d07a
leomozzer <mozzer.ferraz@gmailcom>
2022-05-20 12:03:29 -0300

    Testing docker build in nodejs

--- terraform/app/api/main.tf
+++ terraform/app/api/main.tf
@@ -1,17 +1,5 @@
-# resource "random_string" "random" {
-#   length  = 7
-#   special = false
-#   upper   = false
-#   number  = false
-# }
-
-# locals {
-#   project_name  = "${var.app_name}-${random_string.random.result}"
-#   random_result = random_string.random.result
-# }
-
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.app_name}}-rg-mysql"
+  name     = "${var.app_name}}-rg-api"
   location = "West Europe"
 }
 
@@ -31,32 +19,6 @@ module "acg_api" {
   protocol        = "TCP"
 }
 
-# resource "azurerm_container_group" "example" {
-#   name                = "example-continst"
-#   location            = azurerm_resource_group.example.location
-#   resource_group_name = azurerm_resource_group.example.name
-#   ip_address_type     = "Public"
-#   os_type             = "Linux"
-
-#   image_registry_credential {
-#     username = var.acr_username
-#     password = var.acr_password
-#     server   = var.acr_server
-#   }
-
-#   container {
-#     name   = "api-container"
-#     image  = "zorfexkacr.azurecr.io/api:latest"
-#     cpu    = "0.5"
-#     memory = "1.5"
-
-#     ports {
-#       port     = 9001
-#       protocol = "TCP"
-#     }
-#   }
-
-#   tags = {
-#     environment = "testing"
-#   }
-# }
+output "mysql_ip_address" {
+  value = module.acg_mysql.acg_ip_address
+}
