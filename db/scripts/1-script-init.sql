11eaf509e57c5562047a9451681eccdffe944d4c
leomozzer <mozzer.ferraz@gmail.com>
2022-05-30 10:32:52 -0300

    Updated docker compose and sql scripts

--- db/scripts/1-script-init.sql
+++ db/scripts/1-script-init.sql
@@ -10,29 +10,11 @@ CREATE TABLE IF NOT EXISTS people (
   PRIMARY KEY (id)
 );
 
-INSERT INTO people VALUE(0, 'Adriano Cardoso', 'adriano.cardoso@skaylink.com', '', '');
-INSERT INTO people VALUE(0, 'Andre Espindola', '', '', '');
-INSERT INTO people VALUE(0, 'Alison Pereira Silva', '', 'Support Specialist', '');
-INSERT INTO people VALUE(0, 'Antonio Arnoni', '', '', '');
-INSERT INTO people VALUE(0, 'Bruno Souza',  '', 'Support Specialist', 'Florianopolis');
-INSERT INTO people VALUE(0, 'Carlos Rocha', 'carlos.rocha@skaylink.com', '', '');
-INSERT INTO people VALUE(0, 'Chris Chappuie', '', 'IT Support Coordinator', 'Florianopolis');
-INSERT INTO people VALUE(0, 'Daniel Rosa', 'daniel.rosa@skaylink.com', '', '');
-INSERT INTO people VALUE(0, 'Gabriel Souza', '', '', '');
-INSERT INTO people VALUE(0, 'Igor Rudolph', '', 'Support Analyst', 'Florianopolis');
-INSERT INTO people VALUE(0, 'Joao Vendruscolo', '', '', '');
-INSERT INTO people VALUE(0, 'Jonas Cunha', '', '', '');
-INSERT INTO people VALUE(0, 'Jonas Odorizzi', '', 'Security Analyst', 'Florianopolis');
-INSERT INTO people VALUE(0, 'Karine Oliveira', '', '', '');
-INSERT INTO people VALUE(0, 'Leonardo Benitez', '', '', '');
-INSERT INTO people VALUE(0, 'Leonardo Souza', '', '', '');
-INSERT INTO people VALUE(0, 'Marcelo Santos', '', '', '');
-INSERT INTO people VALUE(0, 'Marcio Konopacki', 'marcio.konopacki@skaylink.com', '', '');
-INSERT INTO people VALUE(0, 'Mateus Petrolli', '', '', '');
-INSERT INTO people VALUE(0, 'Mateus Pioner', '', 'Support Specialist', 'Florianopolis');
-INSERT INTO people VALUE(0, 'Monica Kleesattel', '', '', '');
-INSERT INTO people VALUE(0, 'Rafael Ferreira', '', 'Support Analyst', 'Florianopolis');
-INSERT INTO people VALUE(0, 'Rafael Teixeira', '', 'Security Analyst', 'Florianopolis');
-INSERT INTO people VALUE(0, 'Renan Rendon', 'renan.rendon@skaylink.com', '', '');
-INSERT INTO people VALUE(0, 'Rodrigo Costa', 'rodrigo.costa@skaylink.com', '', '');
-INSERT INTO people VALUE(0, 'Silvio Kleesattel', 'silvio.kleesattel@skaylink.com', '', '');
+INSERT INTO people VALUE(0, 'Luningning Ingmar', 'luningning.ingmar@contoso.com', '', '');
+INSERT INTO people VALUE(0, 'Matty Finn', '', '', '');
+INSERT INTO people VALUE(0, 'Laura Jef', '', 'Support Specialist', '');
+INSERT INTO people VALUE(0, 'Lise Sesto', '', '', '');
+INSERT INTO people VALUE(0, 'Hartwin Aang',  '', 'Support Specialist', 'Springfield');
+INSERT INTO people VALUE(0, 'Aldegund Lena', 'aldegund.lena@contoso.com', '', '');
+INSERT INTO people VALUE(0, 'Gabrijel Torcull', '', 'IT Support Coordinator', 'Springfield');
+INSERT INTO people VALUE(0, 'Cathalán Toufik', 'cathalan.toufik@contoso.com', '', '');
