59ce542dc9703dc68e4d9d190d14481d85397db0
leomozzer <mozzer.ferraz@gmail.com>
2022-07-26 23:43:15 -0300

    Changed acr deployment

--- terraform-modules/acr/variables.tf
+++ terraform-modules/acr/variables.tf
@@ -1,3 +1,11 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
 variable "acr_name" {
   type = string
 }
@@ -6,6 +14,6 @@ variable "acr_sku" {
   type = string
 }
 
-variable "acr_admin_enabled" {
-  type = string
+variable "admin_enabled" {
+  type = bool
 }
