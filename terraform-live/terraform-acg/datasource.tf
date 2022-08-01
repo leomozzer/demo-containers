6cf38cdce3c7c47354ed294f200a4b92889be252
leomozzer <mozzer.ferraz@gmail.com>
2022-08-01 07:40:48 -0300

    removed reconfigure param

--- terraform-live/terraform-acg/datasource.tf
+++ terraform-live/terraform-acg/datasource.tf
@@ -4,3 +4,8 @@ data "azurerm_client_config" "current" {
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
+
+# data "azurerm_container_registry" "acr" {
+#   name                = var.acr_name
+#   resource_group_name = data.azurerm_resource_group.rg.name
+# }
