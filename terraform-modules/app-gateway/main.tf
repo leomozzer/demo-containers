db8ea68d596b65743369feaf879aa8ae5052026a
leomozzer <mozzer.ferraz@gmail.com>
2022-08-21 18:15:27 -0300

    Added appgateway

---
+++ terraform-modules/app-gateway/main.tf
@@ -0,0 +1,56 @@
+
+resource "azurerm_application_gateway" "network" {
+  name                = var.name                # "example-appgateway"
+  resource_group_name = var.resource_group_name # azurerm_resource_group.example.name
+  location            = var.location            #azurerm_resource_group.example.location
+
+  sku {
+    name     = var.sku_name     #"Standard_Small"
+    tier     = var.sku_tier     #"Standard"
+    capacity = var.sku_capacity #2
+  }
+
+  gateway_ip_configuration {
+    name      = var.gateway_ip_configuration_name      #"my-gateway-ip-configuration"
+    subnet_id = var.gateway_ip_configuration_subnet_id #azurerm_subnet.frontend.id
+  }
+
+  frontend_port {
+    name = var.frontend_port_name #local.frontend_port_name
+    port = var.frontend_port      #80
+  }
+
+  frontend_ip_configuration {
+    name                 = var.frontend_ip_configuration_name                 #local.frontend_ip_configuration_name
+    public_ip_address_id = var.frontend_ip_configuration_public_ip_address_id #azurerm_public_ip.example.id
+  }
+
+  backend_address_pool {
+    name = var.backend_address_pool_name #local.backend_address_pool_name
+  }
+
+  backend_http_settings {
+    name                  = var.backend_http_settings_name                  #local.http_setting_name
+    cookie_based_affinity = var.backend_http_settings_cookie_based_affinity #"Disabled"
+    path                  = var.backend_http_settings_path                  # "/path1/"
+    port                  = var.backend_http_settings_port                  #80
+    protocol              = var.backend_http_settings_protocol              #"Http"
+    request_timeout       = var.backend_http_settings_request_timeout       #60
+  }
+
+  http_listener {
+    name                           = var.http_listener_name               #local.listener_name
+    frontend_ip_configuration_name = var.frontend_ip_configuration_name   #local.frontend_ip_configuration_name
+    frontend_port_name             = var.http_listener_frontend_port_name #local.frontend_port_name
+    protocol                       = var.http_listener_protocol           #"Http"
+  }
+
+  request_routing_rule {
+    name                       = var.request_routing_rule_name                  #local.request_routing_rule_name
+    rule_type                  = var.request_routing_rule_type                  #"Basic"
+    http_listener_name         = var.request_routing_http_listener_name         #local.listener_name
+    backend_address_pool_name  = var.request_routing_backend_address_pool_name  #local.backend_address_pool_name
+    backend_http_settings_name = var.request_routing_backend_http_settings_name #local.http_setting_name
+    priority                   = var.request_routing_rule_priority
+  }
+}
---
+++ terraform-modules/app-gateway/variables.tf
@@ -0,0 +1,115 @@
+variable "name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "sku_name" {
+  type = string
+}
+
+variable "sku_tier" {
+  type = string
+}
+
+variable "sku_capacity" {
+  type    = number
+  default = 1
+}
+
+variable "gateway_ip_configuration_name" {
+  type = string
+}
+variable "gateway_ip_configuration_subnet_id" {
+  type = string
+}
+
+variable "frontend_port_name" {
+  type = string
+}
+
+variable "frontend_port" {
+  type = number
+}
+
+variable "frontend_ip_configuration_name" {
+  type = string
+}
+
+variable "frontend_ip_configuration_public_ip_address_id" {
+  type = string
+}
+
+variable "backend_address_pool_name" {
+  type = string
+}
+
+variable "backend_http_settings_name" {
+  type = string
+}
+
+variable "backend_http_settings_cookie_based_affinity" {
+  type = string
+}
+
+variable "backend_http_settings_path" {
+  type = string
+}
+
+variable "backend_http_settings_port" {
+  type = number
+}
+
+variable "backend_http_settings_protocol" {
+  type = string
+}
+
+variable "backend_http_settings_request_timeout" {
+  type = number
+}
+
+variable "http_listener_name" {
+  type = string
+}
+
+variable "http_listener_frontend_ip_configuration_name" {
+  type = string
+}
+
+variable "http_listener_frontend_port_name" {
+  type = string
+}
+
+variable "http_listener_protocol" {
+  type = string
+}
+
+variable "request_routing_rule_name" {
+  type = string
+}
+
+variable "request_routing_rule_type" {
+  type = string
+}
+
+variable "request_routing_http_listener_name" {
+  type = string
+}
+
+variable "request_routing_backend_address_pool_name" {
+  type = string
+}
+
+variable "request_routing_backend_http_settings_name" {
+  type = string
+}
+
+variable "request_routing_rule_priority" {
+  type = number
+}
